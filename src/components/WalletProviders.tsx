3563b4ea1a8e4cb6334921bc9514acb3b7222bcc
httpjunkie <eric@httpjunkie.com>
2024-07-26 20:26:26 -0400

    more cleanup of the `WalletProvider` component

--- src/components/WalletProviders.tsx
+++ src/components/WalletProviders.tsx
@@ -5,28 +5,31 @@ import { formatAddress } from '~/utils'
 import { FaRegCircleCheck } from "react-icons/fa6"
 import { MdLogout } from "react-icons/md"
 
+type Accounts = string[] | undefined;
+
 export const WalletProviders = () => {
   const [selectedWallet, setSelectedWallet] = useState<EIP6963ProviderDetail>()
   const [userAccount, setUserAccount] = useState<string>('')
-  const [metamaskConnected, setMetamaskConnected] = useState<boolean>(false)
   const providers = useSyncProviders()
 
+  console.log(providers)
+
+  const [metamaskConnected, setMetamaskConnected] = useState<boolean>(false)
+
   const handleConnect = async (providerWithInfo: EIP6963ProviderDetail) => {
-    const accounts: string[] | undefined =
-      await (
-        providerWithInfo.provider
-          .request({ method: 'eth_requestAccounts' })
-          .catch(console.error)
-      ) as string[] | undefined;
+    const accounts: Accounts = await (
+      providerWithInfo.provider
+        .request({ method: 'eth_requestAccounts' })
+        .catch(console.error)
+    ) as Accounts;
 
     if (accounts?.[0]) {
       setSelectedWallet(providerWithInfo)
       setUserAccount(accounts?.[0])
     }
     if (providerWithInfo.info.name.toLowerCase().includes("metamask") && accounts?.[0]) {
-      setMetamaskConnected(providerWithInfo.info.name.toLowerCase().includes("metamask") ? true : false)
+      setMetamaskConnected(providerWithInfo.info.name.toLowerCase().includes("metamask"))
     }
-
   }
 
   const handleDisconnect = async (providerWithInfo: EIP6963ProviderDetail) => {
@@ -35,7 +38,6 @@ export const WalletProviders = () => {
         method: 'wallet_revokePermissions',
         params: [{ eth_accounts: {} }]
       })
-
       if (selectedWallet?.info.name.toLowerCase().includes("metamask")) {
         setSelectedWallet(undefined)
         setUserAccount('')
@@ -55,10 +57,11 @@ export const WalletProviders = () => {
         {providers.length > 0 ? providers?.map((provider: EIP6963ProviderDetail) => (
           <div key={provider.info.uuid} className={styles.walletButtons}>
             <button
+              onClick={() => handleConnect(provider)}
               disabled={selectedWallet?.info.uuid === provider.info.uuid}
               className={`${styles.walletButton} ${selectedWallet?.info.uuid !== provider.info.uuid ? styles.notSelected : ''}`}
-              onClick={() => handleConnect(provider)}
-              title={`${selectedWallet?.info.uuid === provider.info.uuid ? provider.info.name + ' Selected' : 'Select ' + provider.info.name}`}>
+              title={`${selectedWallet?.info.uuid === provider.info.uuid ? provider.info.name + ' Selected' : 'Select ' + provider.info.name}`}
+            >
               <img src={provider.info.icon} alt={provider.info.name} />
               {selectedWallet?.info.uuid === provider.info.uuid
                 ? <>
@@ -68,8 +71,7 @@ export const WalletProviders = () => {
                 : <div>{provider.info.name}</div>
               }
             </button>
-            {provider.info.name.toLowerCase().includes("metamask") && (selectedWallet?.info.uuid === provider.info.uuid
-              || (selectedWallet?.info.uuid !== provider.info.uuid && metamaskConnected)) &&
+            {provider.info.name.toLowerCase().includes("metamask") && metamaskConnected &&
               <button className={styles.iconButton} onClick={() => handleDisconnect(provider)} title="Disconnect / Revoke Permission">
                 <MdLogout />
               </button>
